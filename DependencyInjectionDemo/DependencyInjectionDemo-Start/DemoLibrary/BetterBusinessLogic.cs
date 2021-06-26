6ae633deb37d412c7ca809ad38b6c0a369010d52
Abhiroop Mukherjee <abhiroop1990@gmail.com>
2021-06-26 15:16:49 +0530

    Dependency Injection Demo
    Dependency Injection Demo

---
+++ DependencyInjectionDemo/DependencyInjectionDemo-Start/DemoLibrary/BetterBusinessLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DemoLibrary.Utilities;
+
+namespace DemoLibrary
+{
+    public class BetterBusinessLogic : IBusinessLogic
+    {
+        private ILogger _logger;
+        private IDataAccess _dataAccess;
+        public BetterBusinessLogic(ILogger logger, IDataAccess dataAccess)
+        {
+            _logger = logger;
+            _dataAccess = dataAccess;
+        }
+        public void ProcessData()
+        {
+
+
+            _logger.Log("BB: Starting the processing of data.");
+            Console.WriteLine("BB: Processing the data");
+            _dataAccess.LoadData();
+            _dataAccess.SaveData("BB: ProcessedInfo");
+            _logger.Log("BB: Finished processing of the data.");
+        }
+    }
+}
